19d5e5acb4f1a71a63147d87bb89a050a7f5ee81
huhuang03 <huhuang03@gmail.com>
2023-12-03 16:35:56 +0800

    chorn: clean code

--- code/test_on_video.py
+++ code/test_on_video.py
@@ -1,28 +1,27 @@
-import sys, os, argparse
+import argparse
+import os
+import sys
 
-import numpy as np
 import cv2
-import matplotlib.pyplot as plt
-
 import torch
-import torch.nn as nn
-from torch.autograd import Variable
-from torch.utils.data import DataLoader
-from torchvision import transforms
 import torch.backends.cudnn as cudnn
-import torchvision
 import torch.nn.functional as F
+import torchvision
 from PIL import Image
+from torch.autograd import Variable
+from torchvision import transforms
 
-import datasets, hopenet, utils
+import hopenet
+import utils
+
 
 def parse_args():
     """Parse input arguments."""
     parser = argparse.ArgumentParser(description='Head pose estimation using the Hopenet network.')
     parser.add_argument('--gpu', dest='gpu_id', help='GPU device id to use [0]',
-            default=0, type=int)
+                        default=0, type=int)
     parser.add_argument('--snapshot', dest='snapshot', help='Path of model snapshot.',
-          default='', type=str)
+                        default='', type=str)
     parser.add_argument('--video', dest='video_path', help='Path of video')
     parser.add_argument('--bboxes', dest='bboxes', help='Bounding box annotations of frames')
     parser.add_argument('--output_string', dest='output_string', help='String appended to output file')
@@ -30,6 +29,7 @@ def parse_args():
     parser.add_argument('--fps', dest='fps', help='Frames per second of source video', type=float, default=30.)
     args = parser.parse_args()
     return args
+
 
 if __name__ == '__main__':
     args = parse_args()
@@ -51,33 +51,33 @@ if __name__ == '__main__':
     # ResNet50 structure
     model = hopenet.Hopenet(torchvision.models.resnet.Bottleneck, [3, 4, 6, 3], 66)
 
-    print 'Loading snapshot.'
+    print('Loading snapshot.')
     # Load snapshot
     saved_state_dict = torch.load(snapshot_path)
     model.load_state_dict(saved_state_dict)
 
-    print 'Loading data.'
+    print('Loading data.')
 
     transformations = transforms.Compose([transforms.Scale(224),
-    transforms.CenterCrop(224), transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+                                          transforms.CenterCrop(224), transforms.ToTensor(),
+                                          transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
 
     model.cuda(gpu)
 
-    print 'Ready to test network.'
+    print('Ready to test network.')
 
     # Test the Model
     model.eval()  # Change model to 'eval' mode (BN uses moving mean/var).
     total = 0
 
-    idx_tensor = [idx for idx in xrange(66)]
+    idx_tensor = [idx for idx in range(66)]
     idx_tensor = torch.FloatTensor(idx_tensor).cuda(gpu)
 
     video = cv2.VideoCapture(video_path)
 
     # New cv2
-    width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))   # float
-    height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT)) # float
+    width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))  # float
+    height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))  # float
 
     # Define the codec and create VideoWriter object
     fourcc = cv2.VideoWriter_fourcc(*'MJPG')
@@ -105,7 +105,7 @@ if __name__ == '__main__':
         line = line.split(' ')
         det_frame_num = int(line[0])
 
-        print frame_num
+        print(frame_num)
 
         # Stop at a certain frame number
         if frame_num > args.n_frames:
@@ -123,13 +123,14 @@ if __name__ == '__main__':
             frame_num += 1
 
         # Start processing frame with bounding box
-        ret,frame = video.read()
+        ret, frame = video.read()
         if ret == False:
             break
-        cv2_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+        cv2_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
         while True:
-            x_min, y_min, x_max, y_max = int(float(line[1])), int(float(line[2])), int(float(line[3])), int(float(line[4]))
+            x_min, y_min, x_max, y_max = int(float(line[1])), int(float(line[2])), int(float(line[3])), int(
+                float(line[4]))
 
             bbox_width = abs(x_max - x_min)
             bbox_height = abs(y_max - y_min)
@@ -146,7 +147,7 @@ if __name__ == '__main__':
             x_max = min(frame.shape[1], x_max)
             y_max = min(frame.shape[0], y_max)
             # Crop face loosely
-            img = cv2_frame[y_min:y_max,x_min:x_max]
+            img = cv2_frame[y_min:y_max, x_min:x_max]
             img = Image.fromarray(img)
 
             # Transform
@@ -168,12 +169,13 @@ if __name__ == '__main__':
             # Print new frame with cube and axis
             txt_out.write(str(frame_num) + ' %f %f %f\n' % (yaw_predicted, pitch_predicted, roll_predicted))
             # utils.plot_pose_cube(frame, yaw_predicted, pitch_predicted, roll_predicted, (x_min + x_max) / 2, (y_min + y_max) / 2, size = bbox_width)
-            utils.draw_axis(frame, yaw_predicted, pitch_predicted, roll_predicted, tdx = (x_min + x_max) / 2, tdy= (y_min + y_max) / 2, size = bbox_height/2)
+            utils.draw_axis(frame, yaw_predicted, pitch_predicted, roll_predicted, tdx=(x_min + x_max) / 2,
+                            tdy=(y_min + y_max) / 2, size=bbox_height / 2)
             # Plot expanded bounding box
             # cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), (0,255,0), 1)
 
             # Peek next frame detection
-            next_frame_num = int(bbox_line_list[idx+1].strip('\n').split(' ')[0])
+            next_frame_num = int(bbox_line_list[idx + 1].strip('\n').split(' ')[0])
             # print 'next_frame_num ', next_frame_num
             if next_frame_num == det_frame_num:
                 idx += 1
